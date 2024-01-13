06b8ed94dd2bba9df8819a677a709dc03ced35a3
Aditya Belgaonkar <adityabelgaonkar.30@gmail.com>
2024-01-14 00:32:18 +0530

    Added For-Each loop explanations in Basics.

---
+++ Basics/6.5_foreach.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    array<int, 5> arr = {1, 2, 3, 4, 5};
+    //making simple array with 5 elements
+
+    for(int x : arr) //(initialisation : name of array)
+    {
+        cout << x <<"\n";
+    }
+}

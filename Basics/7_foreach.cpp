019c0cce84e58f3ca73c325b699ae7f97e7f169a
Aditya Belgaonkar <adityabelgaonkar.30@gmail.com>
2024-01-14 17:20:54 +0530

    Changed numbering for 6-7-8-9

---
+++ Basics/7_foreach.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int arr[5] = {1, 2, 3, 4, 5};
+    //making simple array with 5 elements
+
+    for(int x : arr) //(initialisation : name of array)
+    {
+        cout << x <<"\n";
+    }
+}
---
+++ Basics/9_pointers.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// you can read '*' as 'value at' and '&' as 'address of'.
+
+int main() {
+    int a = 5;
+    int *point_a = &a; 
+    
+    // This is a pointer variable. It stores the address of the variable a.
+    // To declare pointer variable, use the * sign before the variable name.
+
+    cout << *point_a << endl; // 5
+    // This will print the value of a.
+
+    cout << point_a << a << endl; // 0x61ff085 
+    // This will print the address of the variable a and the value of a.
+
+    
+    *point_a = 10; 
+    cout << *point_a << endl; // 10 
+    cout << point_a << a << endl; // 0x61ff0810 
+
+    string x = "Hello There";
+    // here x is a string variable.
+
+    cout << &x; 
+    // This will print the address of the variable x.
+
+}

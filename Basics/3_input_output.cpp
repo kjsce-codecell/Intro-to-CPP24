d941227d80c2c491e28bd1b7f01241212895b0ad
tkshsbcue <kumar.tanay397@gmail.com>
2024-01-14 18:06:11 +0530

    Added Content For 3_input_output.cpp

---
+++ Basics/3_input_output.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    //For taking input we first declare a blank variable in which we are gonna take input in
+    int a;
+    cin >> a;
+    //as you can see we first declared a variable 'a' which is of type int then using cin>> we take input
+    //please just dont be confuzed betweeb >> and << we use >> in cin
+
+    string b;
+    cin >> b;
+    //see how we here took string as an input same way you can take it in char
+
+    cout << a << " " << b;
+    //cout << is use for printing the value into the console
+    //unlike python in default line does not break you need to manually do it
+    //there are 2 ways \n and endl here \n is faster
+    //" " is given because to provide space between the outputs
+
+}
+

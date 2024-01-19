48e0e0a5f0a3e676e6197f600b9f156615106336
tkshsbcue <kumar.tanay397@gmail.com>
2024-01-19 21:54:19 +0530

    fixing minor issues with code

--- algos/bin_search_algo.cpp
+++ algos/bin_search_algo.cpp
@@ -1,34 +1,30 @@
-//let's work on binary search algo
 #include<bits/stdc++.h>
 using namespace std;
-//setting up parameters in the function this function return an integer
-int find(vector<int>&v,int target){
-    int begin = 0;
-    //setting up the inital index
-    int end = v.size()-1;
-   //remid you that cpp is a 0th index language so -1 from the original length would give the length understandable in scope of cpp
-   
-   //see the things is begin could never exceed end in any case
+
+int bin_search(vector<int>&v,int target){
+    int begin =  0;
+    int end = v.size() -1;
     while(begin <= end){
-        int mid = (int)(begin + (end-begin))/2;
-        //(int)here is basically what we call typecasting basically i am doing float division here
+        int mid = (int)(end + (end-begin))/2;
         int mid_val = v[mid];
-        //this is the mid value which we are gonna compare
         if(mid_val == target){
             return target;
+            
+            break;
         }
         else if(mid_val > target){
-            end = mid + 1;
-        }
-        else if(mid_val < target){
             end = mid - 1;
         }
+        else{
+            begin = mid + 1;
+        }
+
     }
-    return -1;
+    return 0;
 }
 
 
 int main(){
-    vector<int> ar = {1,2,3,4,5,6,7,8,9};
-    cout << find(ar,3);
+    vector<int> arr = {1,2,3,4,5,6,7};
+    cout << bin_search(arr,5);
 }

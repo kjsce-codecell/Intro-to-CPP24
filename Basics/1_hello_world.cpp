b64aabd8673e3face0996c827f58e7948f725d5c
Shreyans T <shreyans.tatiya@gmail.com>
2024-01-24 00:41:01 +0530

    Added explanation and made it simpler.

--- Basics/1_hello_world.cpp
+++ Basics/1_hello_world.cpp
@@ -1,13 +1,17 @@
 #include<bits/stdc++.h>
 /*
 It is a library that Incorporates libraries in the program to use cin, cout like commands.
-just like commands where math operations are used we have to include another library i.e
+This line imports a set of tools we might need for our program.
+It's like preparing a toolbox before starting work.
 */
 using namespace std;
 /*
 They are used to organize code into logical groups and to prevent name 
 collisions that can occur especially when the code base include mutiple LIb
 Namespace provide the space where we can define or declare identifier i.e. variable,  method, classes.
+
+This line tells the computer that we'll be using some common tools, 
+and we'll refer to them as 'std' to avoid any confusion.
 */
 
 int main(){
@@ -15,4 +19,5 @@ int main(){
     cout << "Hello World";
     /*cout is use for printing stuff on screen*/
     return 0;
+    // return 0 is like saying "We smashed this program guys, let's wrap it up!"
 }

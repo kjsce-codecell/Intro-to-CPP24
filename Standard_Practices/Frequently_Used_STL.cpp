5066f4e70f4de4e8cde4ae25ff06b768dca61123
Aditya Belgaonkar <adityabelgaonkar.30@gmail.com>
2024-01-25 02:24:38 +0530

    Added Max/Min and Swap function.

--- Standard_Practices/Frequently_Used_STL.cpp
+++ Standard_Practices/Frequently_Used_STL.cpp
@@ -21,6 +21,18 @@ int main()
     {
         cout << x << endl;
     }
+    int biggernum = max(25, 50);
+    int smallernum = min(30, 60);
+
+    cout << "Bigger number is: " << biggernum << endl;
+    cout << "Smaller number is: " << smallernum << endl;
+
+    int a = 5;
+    int b = 10;
+
+    swap(a, b);
+    
+    cout << "a: " << a << " b: " << b << endl;
     
     auto maxnum = *max_element(vec.begin(), vec.end());
     auto minnum = *min_element(vec.begin(), vec.end());

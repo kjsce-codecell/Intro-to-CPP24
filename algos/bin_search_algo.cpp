c7b2b929d153f329b4dedc38945f5af9b9caa4f2
Shreyans T <shreyans.tatiya@gmail.com>
2024-01-26 00:09:49 +0530

    Updated Binary Search Algo .

--- algos/bin_search_algo.cpp
+++ algos/bin_search_algo.cpp
@@ -1,30 +1,36 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int bin_search(vector<int>&v,int target){
-    int begin =  0;
-    int end = v.size() -1;
-    while(begin <= end){
-        int mid = (int)(end + (end-begin))/2;
-        int mid_val = v[mid];
-        if(mid_val == target){
-            return target;
-            
-            break;
+int binarysearch(int arr[], int size, int target) {
+    int begin = 0;
+    int end = size - 1;
+    while (begin <= end) {
+        int mid = begin + (end - begin)/2;
+        if (arr[mid] == target) {
+            return mid;
         }
-        else if(mid_val > target){
+        else if (target < arr[mid]) {
             end = mid - 1;
         }
-        else{
+        else {
             begin = mid + 1;
         }
-
     }
-    return 0;
+    return -1;
 }
 
+int main() {
+    int arr[] = {2, 5, 8, 12, 16, 23, 38, 56, 72, 91};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    int target;
+    cin >> target;
+    int result = binarysearch(arr, size, target);
 
-int main(){
-    vector<int> arr = {1,2,3,4,5,6,7};
-    cout << bin_search(arr,5);
+    if (result != -1) {
+        cout << "Element is present at index " << result << endl;
+    }
+    else {
+        cout << "Element is not present in array" << endl;
+    }
+    return 0;
 }

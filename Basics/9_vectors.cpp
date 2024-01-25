a7a3abbe04a41dd369cdc548037756991117f238
Shreyans T <shreyans.tatiya@gmail.com>
2024-01-25 23:20:59 +0530

    Adi dekh le.

--- Basics/9_vectors.cpp
+++ Basics/9_vectors.cpp
@@ -13,6 +13,18 @@ for(int i = 0; i < ls.size(); i++){
 }
 cout << endl;
 
+// lets see the iterator way of printing a vector 
+
+for(auto x : ls){
+    cout << x <<" ";
+    }
+
+
+// Linear search in vector 
+// Linear search is a searching algorithm that finds the position of a target value within an array or vector.
+
+// vector<int> ls = {1,2,3,4,5};
+
 int to_find;
 cout << "Enter number to find" << endl;
 cin >> to_find;
@@ -28,14 +40,7 @@ for(int i = 0; i < ls.size(); ++i)
 
 cout << index << endl;
 
-//lets see the iterator way of printing an array 
-//Explain the conept of iterator
 
-for(auto x : ls){
-    cout << x <<" ";
-    }
 //we can also create multidiemensional array
 vector<vector<int>> ls_2d = {{1,0,0},{0,1,0},{0,0,1}};
 }
-
-

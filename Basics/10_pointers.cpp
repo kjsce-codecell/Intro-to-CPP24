9307e2214c8ee02316191ce988ad2381d8dd91cc
Aditya Belgaonkar <adityabelgaonkar.30@gmail.com>
2024-01-27 17:53:55 +0530

    Fixed mistakes in Pointers.

--- Basics/10_pointers.cpp
+++ Basics/10_pointers.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 // you can read '*' as 'value at' and '&' as 'address of'.
@@ -7,7 +7,7 @@ int main() {
     int a = 5;
     int *point_a  = &a;
       
-     // This is a pointer variable. It stores the address of the variable a.
+    // This is a pointer variable. It stores the address of the variable a.
     // To declare pointer variable, use the * sign before the variable name.
 
     cout << *point_a << endl; // 5
@@ -16,13 +16,12 @@ int main() {
     cout << point_a << a << endl; // 0x61ff085 
     // This will print the address of the variable a and the value of a.
 
-    
     *point_a = 10; 
     cout << *point_a << endl; // 10 
     cout << point_a << "\n" <<  a << endl; // 0x61ff0810 
 
     string x = "Hello There";
-    // here x is a string variable.
+    // Here x is a string variable.
 
     cout << &x; 
     // This will print the address of the variable x.

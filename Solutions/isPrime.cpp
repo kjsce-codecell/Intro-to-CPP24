85662d1891787f7cec29bf868090237d9957d6b3
Viraj Bhartiya <vlbhartiya@gmail.com>
2024-01-30 11:22:57 +0530

    isPrime.cpp solution

---
+++ Solutions/isPrime.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+bool isPrime(int n)
+{
+    
+    if (n <= 1){
+        return false;
+    }
+  
+    for (int i = 2; i <= n - 1; i++){
+        if (n % i == 0)
+            return false;
+    }
+ 
+    return true;
+}
+ 
+
+int main()
+{
+    int a;
+    cin >> a;
+    if(isPrime(a)){
+      cout << " true\n";
+    } else {
+      cout << " false\n";
+    }
+    
+    return 0;
+}
